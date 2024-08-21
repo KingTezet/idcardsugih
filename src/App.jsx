98966fab37b48b95bdd468669237f1edbe52c7ac
KingTezet <112939910+KingTezet@users.noreply.github.com>
2024-08-22 05:41:05 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,25 @@ import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
 
 extend({ MeshLineGeometry, MeshLineMaterial })
 
-// Definisikan path untuk GLTF dan Texture
-const gltfPath = process.env.PUBLIC_URL + '/assets/idcard.glb';
-const texturePath = process.env.PUBLIC_URL + '/assets/lanyard.png';
-
-// Tambahkan console.log untuk memeriksa path
-console.log("GLTF Path:", gltfPath);
-console.log("Texture Path:", texturePath);
-console.log("PUBLIC_URL:", process.env.PUBLIC_URL);
-
-useGLTF.preload(gltfPath)
-useTexture.preload(texturePath)
-
 export default function App() {
+  // Definisikan path untuk GLTF dan Texture di dalam komponen
+  const gltfPath = process.env.PUBLIC_URL + '/assets/idcard.glb';
+  const texturePath = process.env.PUBLIC_URL + '/assets/lanyard.png';
+
+  // Tambahkan console.log untuk memeriksa path
+  console.log("GLTF Path:", gltfPath);
+  console.log("Texture Path:", texturePath);
+  console.log("PUBLIC_URL:", process.env.PUBLIC_URL);
+
+  // Preload assets
+  useGLTF.preload(gltfPath);
+  useTexture.preload(texturePath);
+
   return (
     <Canvas camera={{ position: [0, 0, 13], fov: 25 }}>
       <ambientLight intensity={Math.PI} />
       <Physics interpolate gravity={[0, -40, 0]} timeStep={1 / 60}>
-        <Band />
+        <Band gltfPath={gltfPath} texturePath={texturePath} />
       </Physics>
       <Environment background blur={0.75}>
         <color attach="background" args={['grey']} />
@@ -37,7 +38,7 @@ export default function App() {
   )
 }
 
-function Band({ maxSpeed = 50, minSpeed = 10 }) {
+function Band({ gltfPath, texturePath, maxSpeed = 50, minSpeed = 10 }) {
   const band = useRef(), fixed = useRef(), j1 = useRef(), j2 = useRef(), j3 = useRef(), card = useRef() // prettier-ignore
   const vec = new THREE.Vector3(), ang = new THREE.Vector3(), rot = new THREE.Vector3(), dir = new THREE.Vector3() // prettier-ignore
   const segmentProps = { type: 'dynamic', canSleep: true, colliders: false, angularDamping: 2, linearDamping: 2 }
