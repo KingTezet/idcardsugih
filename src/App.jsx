a6eef2a921b933a3aa12f529c814d414609396f8
KingTezet <112939910+KingTezet@users.noreply.github.com>
2024-08-22 04:49:52 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
 // import { useControls } from 'leva'
 
 extend({ MeshLineGeometry, MeshLineMaterial })
-useGLTF.preload('/src/assets/idcard.glb')
-useTexture.preload('/src/assets/lanyard.png')
+useGLTF.preload('assets/idcard.glb')
+useTexture.preload('assets/lanyard.png')
 
 export default function App() {
   // const { debug } = useControls({ debug: false })
@@ -36,8 +36,8 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
   const band = useRef(), fixed = useRef(), j1 = useRef(), j2 = useRef(), j3 = useRef(), card = useRef() // prettier-ignore
   const vec = new THREE.Vector3(), ang = new THREE.Vector3(), rot = new THREE.Vector3(), dir = new THREE.Vector3() // prettier-ignore
   const segmentProps = { type: 'dynamic', canSleep: true, colliders: false, angularDamping: 2, linearDamping: 2 }
-  const { nodes, materials } = useGLTF('/src/assets/idcard.glb')
-  const texture = useTexture('/src/assets/lanyard.png')
+  const { nodes, materials } = useGLTF('assets/idcard.glb')
+  const texture = useTexture('assets/lanyard.png')
   const { width, height } = useThree((state) => state.size)
   const [curve] = useState(() => new THREE.CatmullRomCurve3([new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()]))
   const [dragged, drag] = useState(false)
