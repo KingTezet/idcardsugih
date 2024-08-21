d7af8811f5ebe10b0596e0c2a10c8bb29a847d6f
KingTezet <112939910+KingTezet@users.noreply.github.com>
2024-08-22 05:11:05 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
 // import { useControls } from 'leva'
 
 extend({ MeshLineGeometry, MeshLineMaterial })
-useGLTF.preload(`${process.env.PUBLIC_URL}/assets/idcard.glb`);
-useTexture.preload(`${process.env.PUBLIC_URL}/assets/lanyard.png`);
+useGLTF.preload(`${process.env.PUBLIC_URL}/assets/idcard.glb`)
+useTexture.preload(`${process.env.PUBLIC_URL}/assets/lanyard.png`)
 
 
 export default function App() {
@@ -37,8 +37,8 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
   const band = useRef(), fixed = useRef(), j1 = useRef(), j2 = useRef(), j3 = useRef(), card = useRef() // prettier-ignore
   const vec = new THREE.Vector3(), ang = new THREE.Vector3(), rot = new THREE.Vector3(), dir = new THREE.Vector3() // prettier-ignore
   const segmentProps = { type: 'dynamic', canSleep: true, colliders: false, angularDamping: 2, linearDamping: 2 }
-  const { nodes, materials } = useGLTF(`${process.env.PUBLIC_URL}/assets/idcard.glb`);
-  const texture = useTexture(`${process.env.PUBLIC_URL}/assets/lanyard.png`);
+  const { nodes, materials } = useGLTF(`${process.env.PUBLIC_URL}/assets/idcard.glb`)
+  const texture = useTexture(`${process.env.PUBLIC_URL}/assets/lanyard.png`)
   const { width, height } = useThree((state) => state.size)
   const [curve] = useState(() => new THREE.CatmullRomCurve3([new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()]))
   const [dragged, drag] = useState(false)
